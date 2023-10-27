bb9d9d9672bf9215a7f89398585b735aa18c73d2
ort <orthodoxplus@ya.ru>
2023-10-27 10:44:46 +0300

    refactoring

--- src/main/scala/main/FrequencyAnalyze.scala
+++ src/main/scala/main/FrequencyAnalyze.scala
@@ -2,26 +2,27 @@ package main
 
 object FrequencyAnalyze {
   // digits, english and russian lower letters
-  val allowedSymbolsCodes: Seq[Int] = Seq(0x30 to 0x39, 0x61 to 0x7A, 0x430 to 0x44F)
+  val allowedSymbols: Set[Char] = Seq(0x30 to 0x39, 0x61 to 0x7A, 0x430 to 0x44F)
     .flatMap(range => range.toSeq)
+    .map(i => i.toChar)
+    .toSet
 
-  val allowedSymbols: Set[Char] = allowedSymbolsCodes.map(i => i.toChar).toSet
-
+  // TODO нужно проверять что символ не входит в allowedSymbols
   val splitSymbol: Char = ' '
 
   def analyze(rows: Iterator[String]): Map[String, Int] =
     rows
-      .flatMap(row => prepareToken(row))
-      .toSeq
+      .flatMap(row => rowToTokens(row))
       .filterNot(s => s == "")
+      .toSeq
       .groupBy(identity)
       .view
       .mapValues(_.size)
       .toMap
 
-  private def prepareToken(token: String): Array[String] =
-    if (token == "") Array()
-    else token.toLowerCase
+  private def rowToTokens(row: String): Array[String] =
+    if (row == "") Array()
+    else row.toLowerCase
       .toCharArray
       .map(ch => if (allowedSymbols.contains(ch)) ch else splitSymbol)
       .mkString
