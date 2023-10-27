92042a3a200500b0b474d57cd911f04567cfe57a
ort <orthodoxplus@ya.ru>
2023-10-27 05:16:58 +0300

    tests

--- build.sbt
+++ build.sbt
@@ -6,3 +6,5 @@ lazy val root = (project in file("."))
   .settings(
     name := "test-task-frequency"
   )
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.15" % Test
---
+++ src/test/scala/FrequencyAnalyzeTest.scala
@@ -0,0 +1,87 @@
+import main.FrequencyAnalyze
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
+
+import scala.io.Source
+import scala.util.Using
+
+class FrequencyAnalyzeTest extends AnyFunSuite {
+  test("map equals with hardcode input") {
+    val result = Map("это" -> 2, "текстовый" -> 1, "файл" -> 1, "второе" -> 1, "предложение" -> 1)
+
+    result shouldBe FrequencyAnalyze.analyze(Seq("Это текстовый файл. Это второе предложение.").iterator)
+  }
+
+  test("map equals with read file") {
+    val result = Map("это" -> 2, "текстовый" -> 1, "файл" -> 1, "второе" -> 1, "предложение" -> 1)
+    val url = getClass.getResource("/test-file-1.txt")
+
+    val calculatedMap = Using(Source.fromURL(url)) {
+      source =>
+        val lines = source.getLines()
+        FrequencyAnalyze.analyze(lines)
+    }.get
+
+    calculatedMap shouldBe result
+  }
+
+  test("map equals with read multiline file") {
+    val result = Map(
+      "пунктуации" -> 1,
+      "нижний" -> 1,
+      "принимает" -> 1,
+      "преобразовывать" -> 1,
+      "к" -> 1,
+      "пробелами" -> 1,
+      "их" -> 2,
+      "утилита" -> 1,
+      "возвращает" -> 1,
+      "разделять" -> 1,
+      "принимать" -> 1,
+      "подсчета" -> 1,
+      "все" -> 1,
+      "читать" -> 1,
+      "параметра" -> 1,
+      "текст" -> 1,
+      "слов" -> 4,
+      "регистр" -> 1,
+      "данных" -> 1,
+      "файлу" -> 1,
+      "файла" -> 1,
+      "итоговую" -> 1,
+      "которая" -> 1,
+      "качестве" -> 2,
+      "в" -> 4,
+      "на" -> 1,
+      "файл" -> 1,
+      "создать" -> 1,
+      "символы" -> 1,
+      "разделенные" -> 1,
+      "возвращать" -> 1,
+      "частоты" -> 2,
+      "входных" -> 1,
+      "фильтровать" -> 1,
+      "файле" -> 1,
+      "текстовый" -> 1,
+      "знаки" -> 1,
+      "путь" -> 1,
+      "слова" -> 2,
+      "содержимое" -> 1,
+      "карту" -> 3,
+      "специальные" -> 1,
+      "утилиту" -> 1,
+      "должна" -> 1,
+      "и" -> 4,
+      "вхождений" -> 1,
+      "создавать" -> 1)
+    val url = getClass.getResource("/test-file-2.txt")
+
+    val calculatedMap = Using(Source.fromURL(url)) {
+      source =>
+        val lines = source.getLines()
+        FrequencyAnalyze.analyze(lines)
+    }.get
+
+    calculatedMap shouldBe result
+  }
+}
