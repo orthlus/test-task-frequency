c9dd7338aef28573619766445b2d46fbb5e2bc08
ort <orthodoxplus@ya.ru>
2023-10-29 17:58:52 +0300

    separate test files

---
+++ src/test/scala/ConsoleTest.scala
@@ -0,0 +1,30 @@
+import main.Main
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
+
+class ConsoleTest extends AnyFunSuite {
+  test("console, args - empty file") {
+    val fileUrl = getClass.getResource("/test-file-3.txt").getFile.substring(1)
+
+    an[RuntimeException] should be thrownBy Main.main(Array(fileUrl))
+
+    val thrown = the[RuntimeException] thrownBy Main.main(Array(fileUrl))
+    thrown.getMessage shouldBe "passed file is empty"
+  }
+
+  test("console, args - not found file") {
+    val fileUrl = "/test-file-0.txt"
+
+    an[RuntimeException] should be thrownBy Main.main(Array(fileUrl))
+
+    val thrown = the[RuntimeException] thrownBy Main.main(Array(fileUrl))
+    thrown.getMessage shouldBe "passed file not found or path is invalid"
+  }
+
+  test("console, args - no args") {
+    an[RuntimeException] should be thrownBy Main.main(Array())
+
+    val thrown = the[RuntimeException] thrownBy Main.main(Array())
+    thrown.getMessage shouldBe "need only 1 argument - local file path"
+  }
+}
